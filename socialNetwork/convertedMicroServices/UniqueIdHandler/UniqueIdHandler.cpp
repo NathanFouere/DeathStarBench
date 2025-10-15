f516c7a8faedee1356af3e11bfc463a0df20a753
Quentin Le_Goff <quentin.legoff44@gmail.com>
2025-10-15 13:57:16 +0200

    Fixing UniqueIdHandler and added some documentation to compile & build project.

--- socialNetwork/convertedMicroServices/UniqueIdHandler/UniqueIdHandler.cpp
+++ socialNetwork/convertedMicroServices/UniqueIdHandler/UniqueIdHandler.cpp
@@ -3,26 +3,20 @@
 
 using namespace emscripten;
 
-UniqueIdHandler::UniqueIdHandler(
-    std::mutex *thread_lock,
-    const std::string &machine_id
-) {
-    _thread_lock = thread_lock;
+UniqueIdHandler::UniqueIdHandler(const std::string &machine_id) {
     _machine_id = machine_id;
+    std::cout << "UniqueIdHandler initialized with machine_id: " << machine_id << std::endl;
 }
 
 int64_t UniqueIdHandler::ComposeUniqueId(
     int64_t req_id,
-    PostType::type post_type,
-    const std::map<std::string, std::string> &carrier
+    PostType::type post_type
 ) {
-    _thread_lock->lock();
     int64_t timestamp =
         duration_cast<milliseconds>(system_clock::now().time_since_epoch())
             .count() -
         CUSTOM_EPOCH;
     int idx = GetCounter(timestamp);
-    _thread_lock->unlock();
 
     std::stringstream sstream;
     sstream << std::hex << timestamp;
@@ -46,82 +40,22 @@ int64_t UniqueIdHandler::ComposeUniqueId(
     } else if (counter_hex.size() < 3) {
       counter_hex = std::string(3 - counter_hex.size(), '0') + counter_hex;
     }
+
     std::string post_id_str = _machine_id + timestamp_hex + counter_hex;
-    int64_t post_id = stoul(post_id_str, nullptr, 16) & 0x7FFFFFFFFFFFFFFF;
-    std::cout << "The post_id of the request " << req_id << " is " << post_id << std::endl;
+    int64_t post_id = stoull(post_id_str, nullptr, 16) & 0x7FFFFFFFFFFFFFFF;
+    std::cout << "Post ID for request " << req_id << " (type: " << post_type << "): " << post_id << std::endl;
 
     return post_id;
 }
 
-uint16_t HashMacAddressPid(const std::string &mac) {
-  uint16_t hash = 0;
-  std::string mac_pid = mac + std::to_string(getpid());
-  for (unsigned int i = 0; i < mac_pid.size(); i++) {
-    hash += (mac[i] << ((i & 1) * 8));
-  }
-  return hash;
-}
-
-int GetMachineId (std::string *mac_hash) {
-  std::string mac;
-  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP );
-  if ( sock < 0 ) {
-    std::cout << "Unable to obtain MAC address" << std::endl;
-    return -1;
-  }
-
-  struct ifconf conf{};
-  char ifconfbuf[ 128 * sizeof(struct ifreq)  ];
-  memset( ifconfbuf, 0, sizeof( ifconfbuf ));
-  conf.ifc_buf = ifconfbuf;
-  conf.ifc_len = sizeof( ifconfbuf );
-  if ( ioctl( sock, SIOCGIFCONF, &conf ))
-  {
-    std::cout << "Unable to obtain MAC address" << std::endl;
-    return -1;
-  }
-
-  struct ifreq* ifr;
-  for (
-      ifr = conf.ifc_req;
-      reinterpret_cast<char *>(ifr) <
-          reinterpret_cast<char *>(conf.ifc_req) + conf.ifc_len;
-      ifr++) {
-    if ( ifr->ifr_addr.sa_data == (ifr+1)->ifr_addr.sa_data ) {
-      continue;  // duplicate, skip it
-    }
-
-    if ( ioctl( sock, SIOCGIFFLAGS, ifr )) {
-      continue;  // failed to get flags, skip it
-    }
-    if ( ioctl( sock, SIOCGIFHWADDR, ifr ) == 0 ) {
-      mac = std::string(ifr->ifr_addr.sa_data);
-      if (!mac.empty()) {
-        break;
-      }
-    }
-  }
-  close(sock);
-
-  std::stringstream stream;
-  stream << std::hex << HashMacAddressPid(mac);
-  *mac_hash = stream.str();
-
-  if (mac_hash->size() > 3) {
-    mac_hash->erase(0, mac_hash->size() - 3);
-  } else if (mac_hash->size() < 3) {
-    *mac_hash = std::string(3 - mac_hash->size(), '0') + *mac_hash;
-  }
-  return 0;
-}
-
-
-
-EMSCRIPTEN_BINDINGS() {
+EMSCRIPTEN_BINDINGS(unique_id_module) {
   class_<UniqueIdHandler>("UniqueIdHandler")
-    .constructor<
-    std::mutex *,
-    const std::string &
-    >()
-  ;
-};
+    .constructor<const std::string&>()
+    .function("ComposeUniqueId", &UniqueIdHandler::ComposeUniqueId);
+
+  enum_<PostType::type>("PostType")
+    .value("POST", PostType::POST)
+    .value("REPOST", PostType::REPOST)
+    .value("REPLY", PostType::REPLY)
+    .value("DM", PostType::DM);
+}
--- socialNetwork/convertedMicroServices/UniqueIdHandler/UniqueIdHandler.hpp
+++ socialNetwork/convertedMicroServices/UniqueIdHandler/UniqueIdHandler.hpp
@@ -3,18 +3,9 @@
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
-#include <mutex>
 #include <sstream>
 #include <string>
 #include <map>
-#include <fstream>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <sys/types.h>
-#include <net/if.h>
-#include <unistd.h>
-#include <cstring>
 
 #define CUSTOM_EPOCH 1514764800000
 
@@ -27,8 +18,8 @@ static int counter = 0;
 
 static int GetCounter(int64_t timestamp) {
   if (current_timestamp > timestamp) {
-    std::cout << "Timestamps are not incremental." << std::endl;
-    exit(EXIT_FAILURE);
+    std::cout << "WARNING: Timestamps are not incremental!" << std::endl;
+    // En JavaScript mono-thread, on log juste l'avertissement sans crasher
   }
   if (current_timestamp == timestamp) {
     return counter++;
@@ -50,18 +41,13 @@ struct PostType {
 
 class UniqueIdHandler {
     public:
-        UniqueIdHandler(
-            std::mutex *thread_lock,
-            const std::string &machine_id
-        );
+        UniqueIdHandler(const std::string &machine_id);
+
         int64_t ComposeUniqueId(
             int64_t req_id,
-            PostType::type post_type,
-            const std::map<std::string, std::string> &carrier
+            PostType::type post_type
         );
-        uint16_t HashMacAddressPid(const std::string &mac);
-        std::string GetMachineId(std::string &netif);
+
     private:
-        std::mutex *_thread_lock;
         std::string _machine_id;
 };
